75216584919237a26360d1575a63fbdc204a9c8f
Adam Reis <adamhreis@gmail.com>
2015-05-09 02:29:06 -0400

    added background pix and main page

--- src/application/forms.py
+++ src/application/forms.py
@@ -15,12 +15,5 @@ from wtforms.ext.appengine.ndb import model_form
 from .models import ExampleModel
 
 
-class ClassicExampleForm(wtf.Form):
-    example_name = wtf.TextField('Name', validators=[validators.Required()])
-    example_description = wtf.TextAreaField('Description', validators=[validators.Required()])
-
-# App Engine ndb model form example
-ExampleForm = model_form(ExampleModel, wtf.Form, field_args={
-    'example_name': dict(validators=[validators.Required()]),
-    'example_description': dict(validators=[validators.Required()]),
-})
+class SignupForm(wtf.Form):
+    email = wtf.TextField('Email Address', [validators.Length(min=6, max=35)])
--- src/application/urls.py
+++ src/application/urls.py
@@ -18,26 +18,11 @@ app.add_url_rule('/_ah/warmup', 'warmup', view_func=views.warmup)
 # Home page
 app.add_url_rule('/', 'home', view_func=views.home)
 
-# Say hello
-app.add_url_rule('/hello/<username>', 'say_hello', view_func=views.say_hello)
-
-# Examples list page
-app.add_url_rule('/examples', 'list_examples', view_func=views.list_examples, methods=['GET', 'POST'])
-
-# Examples list page (cached)
-app.add_url_rule('/examples/cached', 'cached_examples', view_func=views.cached_examples, methods=['GET'])
-
-# Contrived admin-only view example
-app.add_url_rule('/admin_only', 'admin_only', view_func=views.admin_only)
-
-# Edit an example
-app.add_url_rule('/examples/<int:example_id>/edit', 'edit_example', view_func=views.edit_example, methods=['GET', 'POST'])
-
-# Delete an example
-app.add_url_rule('/examples/<int:example_id>/delete', view_func=views.delete_example, methods=['POST'])
-
 # Download all the shit
 app.add_url_rule('/scrape', view_func=views.scrape_everything, methods=['GET', 'POST'])
+
+# Add new user
+app.add_url_rule('/signup', view_func=views.signup, methods=['POST'])
 
 ## Error handlers
 # Handle 404 errors
